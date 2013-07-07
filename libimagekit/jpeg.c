cb3aad805e0fa3d550abd1cbbea77347db0a0eea
Cameron Eure <turbinater@gmail.com>
2013-07-07 11:53:11 -0700

    Cleanup

--- libimagekit/jpeg.c
+++ libimagekit/jpeg.c
@@ -136,79 +136,10 @@ ImageKit_Image_FromJPEG(const char *filepath, REAL scale)
     return self;
 }
 
-#if 0
-
-int write_jpeg_file( char *filename )
-{
-    struct jpeg_compress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-
-    /* this is a pointer to one row of image data */
-    JSAMPROW row_pointer[1];
-    FILE *outfile = fopen( filename, "wb" );
-
-    if ( !outfile )
-    {
-        printf("Error opening output jpeg file %s\n!", filename );
-        return -1;
-    }
-    
-    cinfo.err = jpeg_std_error( &jerr );
-    jpeg_create_compress(&cinfo);
-    jpeg_stdio_dest(&cinfo, outfile);
-
-    /* Setting the parameters of the output file here */
-    cinfo.image_width = width;
-    cinfo.image_height = height;
-    cinfo.input_components = bytes_per_pixel;
-    cinfo.in_color_space = color_space;
-    /* default compression parameters, we shouldn't be worried about these */
-
-    jpeg_set_defaults( &cinfo );
-    cinfo.num_components = 3;
-    //cinfo.data_precision = 4;
-    cinfo.dct_method = JDCT_FLOAT;
-    jpeg_set_quality(&cinfo, 15, TRUE);
-    /* Now do the compression .. */
-    jpeg_start_compress( &cinfo, TRUE );
-    /* like reading a file, this time write one row at a time */
-    while( cinfo.next_scanline < cinfo.image_height )
-    {
-        row_pointer[0] = &raw_image[ cinfo.next_scanline * cinfo.image_width * cinfo.input_components];
-        jpeg_write_scanlines( &cinfo, row_pointer, 1 );
-    }
-    
-    /* similar to read file, clean up after we're done compressing */
-    jpeg_finish_compress( &cinfo );
-    jpeg_destroy_compress( &cinfo );
-    fclose( outfile );
-    
-    /* success code is 1! */
-    return 1;
-}
-
-#endif
-
 API
 int
 ImageKit_Image_SaveJPEG(ImageKit_Image *self, const char *filepath, int quality)
 {
-    // FIXME
-    
-    /*
-enum {
-    ImageKit_NotImplementedError,
-    ImageKit_StandardError,
-    ImageKit_ValueError,
-    ImageKit_TypeError,
-    ImageKit_OSError,
-    ImageKit_IOError,
-    ImageKit_MemoryError,
-    ImageKit_IndexError,
-    IMAGEKIT_NUM_ERRORS
-} IMAGEKIT_ERROR;
-    */
-    
     ImageKit_Image *copy = NULL;
     
     /* JPEG variables */
