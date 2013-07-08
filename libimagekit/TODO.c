9ce293570ee035c90e818bd47def26f5aa983a8a
Cameron Eure <turbinater@gmail.com>
2013-07-08 10:06:26 -0700

    Updated TODO

--- libimagekit/TODO.c
+++ libimagekit/TODO.c
@@ -7,5 +7,7 @@ TODO List
 - Non-RGB colorspaces for JPEG
 - Ability to set bit depth in SavePNG()
 - RGB-30 in SavePNG
+- Better and more complete tests
+- Test alpha support on RGB48 and RGB30 images, in SavePNG()
 
 */
