989eb71ffbe787ff7cc3f9debd27501fff273ccf
Cameron Eure <turbinater@gmail.com>
2013-07-08 10:45:01 -0700

    Updated TODO

--- libimagekit/TODO.c
+++ libimagekit/TODO.c
@@ -3,11 +3,23 @@
 
 TODO List
 
+- Histogram
+- Blit / Transpose
+- Get box?
 - JPEG Grayscale support
 - Non-RGB colorspaces for JPEG
 - Ability to set bit depth in SavePNG()
 - RGB-30 in SavePNG
 - Better and more complete tests
 - Test alpha support on RGB48 and RGB30 images, in SavePNG()
+- Matrix
+- Convolution kernel
+- Rank filter
+- Bilateral filter
+- Resize, nearest, bilinear, cubic family (B-Spline, Mitchell and Catmull-Rom), Lanczos3
+- Rotate
+- Point Functions
+    - brightness / contrast
+    - gamma
 
 */
