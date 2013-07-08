996957ef46dc97b34162955ead5f41970f370dea
Cameron Eure <turbinater@gmail.com>
2013-07-08 10:02:03 -0700

    Updated TODO

--- libimagekit/TODO.c
+++ libimagekit/TODO.c
@@ -6,7 +6,6 @@ TODO List
 - JPEG Grayscale support
 - Non-RGB colorspaces for JPEG
 - Ability to set bit depth in SavePNG()
-- Convert between RGB formats in ImageKit_Image_toRGB()
 - RGB-30 in SavePNG
 
 */
