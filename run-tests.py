6954345e46117bf2ccf594b28643ab1614182738
Cameron Eure <turbinater@gmail.com>
2013-10-27 17:03:25 -0700

    Implemented run-tests.py

--- run-tests.py
+++ run-tests.py
@@ -1,5 +1,18 @@
 
 import os, sys, subprocess
 
+def run_tests(path):
+    for item in os.listdir(os.path.join(*path)):
+        abs = os.path.join(*path + [item])
+        if os.path.isdir(abs):
+            run_tests(list(path) + [item])
+        elif abs.endswith('.py'):
+            p = subprocess.Popen(['python', abs])
+            p.communicate()
+            if not p.returncode == 0:
+                sys.stderr.write('%s: Failed (Return Code %s)\n' % (abs, p.returncode))
+            else:
+                sys.stdout.write('%s: Succeeded\n' % (abs))
+
 if __name__ == '__main__':
-    pass
+    run_tests(['tests'])
